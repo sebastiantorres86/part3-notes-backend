0e8ce2aa106c50405a37dc219aa33b58c5456e53
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-10-25 21:39:39 -0300

    part4-9

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-undef */
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
@@ -8,10 +10,23 @@ notesRouter.get('/', async (request, response) => {
   response.json(notes)
 })
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id)
 
   const note = new Note({
     content: body.content,
